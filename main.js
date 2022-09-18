87e7c7852fdef06929ee5b1bbb0077f3d19e9f69
KenpachiPN <sprada71@gmail.com>
2022-09-18 10:11:20 -0500

    Ejercicio #1

---
+++ main.js
@@ -0,0 +1,12 @@
+
+let num = Number(prompt("Ingrese un número"));
+    if (num < 100 ) {
+        alert("El número " + num + " es positivo y menor a 100");
+}
+    else if (num < 0){
+        alert("El número " + num + " es negativo y menor que 100 ")
+    }
+    
+       
+
+    
